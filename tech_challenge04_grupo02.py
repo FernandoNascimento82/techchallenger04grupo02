92ea42492366fd34b8cce0e9a94467224c796dd9
FernandoNascimento82 <136522189+FernandoNascimento82@users.noreply.github.com>
2024-01-28 21:58:21 -0300

    Update tech_challenge04_grupo02.py

--- tech_challenge04_grupo02.py
+++ tech_challenge04_grupo02.py
@@ -96,18 +96,6 @@ st.write("")
 st.write("")
 
 
-df_crise = df[(df.index >= '2023-08-01') & (df.index <= '2023-09-31')]
-grafico = plt.figure(figsize=(10, 6))
-plt.plot(df_crise)
-font_grafico = {'family':'serif','color':'darkred','size':20}
-plt.title("Queda no estoque do combustível fóssil eleva preço do petróleo em 2023", fontdict=font_grafico, weight='bold', style='italic')
-plt.legend(bbox_to_anchor = (1.3, 1.3), ncol = 8)
-plt.xlabel("Periodo")
-plt.ylabel("Valor Total")
-plt.xticks(rotation=35)
-st.pyplot(grafico) 
-
-
 st.write("---")
 st.write("")
 st.write("")
